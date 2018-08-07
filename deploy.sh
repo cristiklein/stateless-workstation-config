423c079a7e4b4cefe34152f7e4b9d19dad245fb9
Cristian Klein <cristiklein@gmail.com>
2018-08-07 14:51:37 +0200

    Added informative failure message

--- deploy.sh
+++ deploy.sh
@@ -13,10 +13,14 @@ if ! which ansible > /dev/null; then
     sudo apt-get update
     sudo apt-get install ansible
 fi
-    
-env \
+
+if ! env \
     LANG=C.UTF-8 \
     ANSIBLE_STDOUT_CALLBACK=debug \
     ANSIBLE_CONFIG=$BASEDIR/ansible.cfg \
     ansible-playbook $BASEDIR/site.yml \
         --inventory localhost, $*
+then
+    log "If the failure above is due to missing privilege, then re-run with:"
+    log "$0 --ask-become-pass"
+fi
