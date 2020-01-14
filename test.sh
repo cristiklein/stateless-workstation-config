eac09645deb765cebc850ff49b7f71fe0c6a205e
Cristian Klein <cristian@kleinlabs.eu>
2020-01-14 22:32:29 +0100

    Add test script

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_IMAGE=ubuntu-minimal
+
+docker build -t $DOCKER_IMAGE - <<EOF
+FROM ubuntu:19.10
+
+ENV DEBIAN_FRONTEND=noninteractive
+RUN \
+    apt-get update \
+    && apt-get install -yyq ubuntu-minimal \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+RUN useradd \
+    --create-home \
+    --groups sudo \
+    blah
+USER blah:blah
+EOF
+
+docker run \
+    -ti \
+    -v $(pwd):$(pwd):ro \
+    -w $(pwd) \
+    $DOCKER_IMAGE bash --login ./deploy.sh --skip-tags snap,systemd
